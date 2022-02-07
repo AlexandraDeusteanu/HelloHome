190b4437fa3f4b6b7b80deecef962197f6291bb9
Your Name <dallexandra30@gmail.com>
2022-02-07 17:05:30 +0200

    add isAuthor

--- middleware.js
+++ middleware.js
@@ -1,4 +1,8 @@
-const isLoggedIn = (req, res, next) => {
+const { campgroundSchema } = require("./schemas.js");
+const ExpressError = require("./utilities/ExpressError")
+const Campground = require("./models/campground")
+
+module.exports.isLoggedIn = (req, res, next) => {
   
     if(!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
@@ -8,4 +12,23 @@ const isLoggedIn = (req, res, next) => {
     next();
 }
 
-module.exports = isLoggedIn;
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(",")
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
+    if(!campground.author.equals(req.user._id)) {
+        req.flash("error", "You do not have permission to do that!");
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
